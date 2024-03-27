8e8f57261db9bcaab0649ec5972f1a9ee604021a
Eugeni Bykovski <eugeni.bykovski@itexus.com>
2024-03-27 14:43:23 +0100

    added fonts, header and footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,16 @@
+import "./globals.css";
+import { DM_Sans } from "next/font/google";
+
 import { PrismicPreview } from "@prismicio/next";
 import { repositoryName } from "@/prismicio";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+
+export const dmSans = DM_Sans({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-dm-sans",
+});
 
 export default function RootLayout({
   children,
@@ -7,8 +18,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" className={`${dmSans.variable}`}>
+      <body>
+        <Header />
+        <main>{children}</main>
+        <Footer />
+      </body>
       <PrismicPreview repositoryName={repositoryName} />
     </html>
   );
